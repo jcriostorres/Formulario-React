a0cf74e237c009f007d9f95105b64ae76d278ed2
Santiago Franco <ktns0930@gmail.com>
2021-09-13 22:17:44 -0500

    Se generaron validaciones basicas y se obtienen valores validados.

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,11 @@
 import React, {useState, Fragment} from 'react';
-
+import { useForm } from 'react-hook-form';
 const Formulario = () => {
-
     
+    const {register, errors, handleSubmit} = useForm();
+    
+    const [entradas, setEntradas] = useState([])
+
     const[usuario, setUsuario] = useState({
         nombre: '',
         edad: '',
@@ -19,7 +22,10 @@ const Formulario = () => {
     const registrarUsuario = (event) => {
         event.preventDefault()
         console.log(usuario.nombre + '  ' +  usuario.edad + '  ' + usuario.ocupacion);
+    }
 
+    const onSubmit = (data, e) =>{
+        console.log(data);
     }
 
 
@@ -27,32 +33,29 @@ const Formulario = () => {
     return ( 
         <Fragment>
             <h1>Registro de Usuarios</h1>
-            <form className="row" onSubmit = {registrarUsuario}>
+            <form className="row" onSubmit = {handleSubmit(onSubmit)}>
                 <div className="col-md-3">
                     <input 
                         type = "text" 
                         placeholder="Ingrese Nombre" 
                         className="form-control"
                         name = "nombre"
-                        onChange = {handleInputChange}
-                    ></input>
+                        {...register("nombre",{required: true})}/>
                 </div>
-
             <div className="col-md-3">
                     <input
-                        type="text"
+                        type="number"
                         placeholder="Ingrese Edad"
                         className="form-control"
                         name = "edad"
-                        onChange = {handleInputChange}
-
+                        {...register("edad",{required: true, min: 1})}
                     ></input>
             </div>
 
 
             <div className="col-md-3">
                 
-            <select type = "text" name="ocupacion"  className="form-control" onChange = {handleInputChange}>
+            <select type = "text" name="ocupacion"  className="form-control" {...register("ocupacion",{required: true})}>
                 <option value="empleado" selected>Seleccione ocupacion...</option>
                 <option value="estudiante"> Estudiante</option>
                 <option value="empleado">Empleado</option>
