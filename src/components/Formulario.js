d4cff7a04a4ab28530ab4f35aa09807c4b884e60
jocarito <jcriostorres@gmail.com>
2021-09-13 19:34:23 -0500

    Se agrego funcion handleInputChange y hook de usuario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,36 +3,49 @@ import React, {useState, Fragment} from 'react';
 const Formulario = () => {
 
     
-    const [usuario, setUsuario] = useState({
-        nombre:'',
-        edad:'',
-        ocupacion: ''
+    const[usuario, setUsuario] = useState({
+        nombre: '',
+        edad: ''
     })
+
+    const handleInputChange = (event) =>{
+        setUsuario({
+            ...usuario,
+            [event.target.name] : event.target.value
+        })
+    }
+
+
 
     return ( 
         <Fragment>
             <h1>Registro de Usuarios</h1>
             <form className="row">
                 <div className="col-md-3">
-                    <input  
-                    placeholder="Ingrese Nombre" 
-                    className="form-control"
-                    type="text"
+                    <input 
+                        type = "text" 
+                        placeholder="Ingrese Nombre" 
+                        className="form-control"
+                        name = "nombre"
+                        onChange = {handleInputChange}
                     ></input>
                 </div>
 
             <div className="col-md-3">
                     <input
-                    placeholder="Ingrese Edad"
-                    className="form-control"
-                    type="text"
+                        type="text"
+                        placeholder="Ingrese Edad"
+                        className="form-control"
+                        name = "edad"
+                        onChange = {handleInputChange}
+
                     ></input>
             </div>
 
 
             <div className="col-md-3">
                 
-            <select name="ocupacion"  className="form-control">
+            <select type = "text" name="ocupacion"  className="form-control" onChange = {handleInputChange}>
                 <option value="empleado" selected>Seleccione ocupacion...</option>
                 <option value="estudiante"> Estudiante</option>
                 <option value="empleado">Empleado</option>
@@ -40,10 +53,11 @@ const Formulario = () => {
             </select>
             </div> 
 
-            <button className="btn btn-primary" type="submit">guardar</button>
-            
+            <div className = "col-md-3">
+                <button className="btn btn-primary" type="submit">guardar</button>
+            </div>
             </form>
-
+            <h3>{usuario.nombre} - {usuario.edad} - {usuario.ocupacion}</h3>
         </Fragment>
      );
 }
